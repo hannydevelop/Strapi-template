d630aa8898882665bafe3d5d4a09f2e7b277569c
Ukpai Ugochi <62933980+hannydevelop@users.noreply.github.com>
2021-06-21 08:47:51 +0100

    Update bootstrap.js

--- template/config/functions/bootstrap.js
+++ template/config/functions/bootstrap.js
@@ -1,5 +1,4 @@
-'use strict';
-
+"use strict";
 /**
  * An asynchronous bootstrap function that runs before
  * your application gets started.
@@ -7,42 +6,43 @@
  * This gives you an opportunity to set up your data model,
  * run jobs, or perform some special logic.
  *
- * See more details here: https://strapi.io/documentation/developer-docs/latest/setup-deployment-guides/configurations.html#bootstrap
+ * See more details here: https://strapi.io/documentation/3.0.0-beta.x/concepts/configurations.html#bootstrap
  */
-
-module.exports = () => {
-  
-async function setPublicPermissions(newPermissions) {
-  // Find the ID of the public role
-  const publicRole = await strapi
+const findPublicRole = async () => {
+  const result = await strapi
     .query("role", "users-permissions")
     .findOne({ type: "public" });
-  // List all available permissions
-  const publicPermissions = await strapi
+  return result;
+};
+
+const setDefaultPermissions = async () => {
+  const role = await findPublicRole();
+  const permissions = await strapi
     .query("permission", "users-permissions")
-    .find({
-      type: ["users-permissions", "application"],
-      role: publicRole.id,
-    });
-  // Update permission to match new config
-  const controllersToUpdate = Object.keys(newPermissions);
-  const updatePromises = publicPermissions
-    .filter((permission) => {
-      // Only update permissions included in newConfig
-      if (!controllersToUpdate.includes(permission.controller)) {
-        return false;
-      }
-      if (!newPermissions[permission.controller].includes(permission.action)) {
-        return false;
-      }
-      return true;
-    })
-    .map((permission) => {
-      // Enable the selected permissions
-      return strapi
+    .find({ type: "application", role: role.id });
+  await Promise.all(
+    permissions.map(p =>
+      strapi
         .query("permission", "users-permissions")
-        .update({ id: permission.id }, { enabled: true });
-    });
-  await Promise.all(updatePromises);
-}
+        .update({ id: p.id }, { enabled: true })
+    )
+  );
+};
+
+const isFirstRun = async () => {
+  const pluginStore = strapi.store({
+    environment: strapi.config.environment,
+    type: "type",
+    name: "setup"
+  });
+  const initHasRun = await pluginStore.get({ key: "initHasRun" });
+  await pluginStore.set({ key: "initHasRun", value: true });
+  return !initHasRun;
+};
+
+module.exports = async () => {
+  const shouldSetDefaultPermissions = await isFirstRun();
+  if (shouldSetDefaultPermissions) {
+    await setDefaultPermissions();
+  }
 };
